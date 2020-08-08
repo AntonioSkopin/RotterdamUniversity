bc0b944e0eb972e96ceac44eb950db1c05c104dd
AntonioSkopin <askopin@outlook.com>
2020-08-08 20:27:23 +0200

    School site with CRUD operations

---
+++ Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RotterdamUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace RotterdamUniversity.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RotterdamUniversity.Data.SchoolContext _context;
+
+        public IndexModel(RotterdamUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Students { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Students = await _context.Students.ToListAsync();
+        }
+    }
+}
